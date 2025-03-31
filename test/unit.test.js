ff9c8213e3178dd6890892fb8739f60f96d1baaa
pandajakob <jakobm.dev@gmail.com>
2025-03-31 11:40:24 +0200

    commit

--- test/unit.test.js
+++ test/unit.test.js
@@ -1,12 +1,3 @@
-import "../backend/server.js"
-import "../backend/routes/userRoutes.js"
-import "../backend/routes/gameRoutes.js"
-import "../backend/controllers/gameController.js"
-import "../backend/controllers/userController.js"
-import {login, registerUser} from "../frontend/scripts/userLogic/userFunctions.js"
-import {fetchGameData, deleteGame, submitShips, fireShot} from "../frontend/scripts/gameLogic/gameFunctions.js"
-
-
 import { assert, expect, test, it } from "vitest";
 
 
