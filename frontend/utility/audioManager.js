2075381dbe537adfd72451134825f33a20b70fe9
pandajakob <jakobm.dev@gmail.com>
2025-04-02 16:13:02 +0200

    fix music and volume.
    nu kan man da skrue op og ned for musikken og den gemmer det i local storage til næste gang

--- frontend/utility/audioManager.js
+++ frontend/utility/audioManager.js
@@ -1,20 +1,25 @@
 /** @module audioManager */
-
-
-var click = new Howl({
-    src: ["../resources/sounds/mouse_click.wav"],
-    volume: 0.1,
+import "https://cdn.jsdelivr.net/npm/howler@2.2.3/dist/howler.min.js";
+import { volume } from "./state.js";
+export let clickSound = new Howl({
+    src: ["/resources/sounds/mouse_click.wav"],
+    volume: 0.01,
 });
 
-var music = new Howl({
-    src: ["../resources/sounds/music1.mp3"],
-    volume: 0.5,
+
+export let music = new Howl({
+    src: ["/resources/sounds/music1.mp3"],
+    volume: 0,
     loop: true,
     autoplay: true
 })
 
 document.addEventListener("click", () => {
-    click.play();
+    clickSound.play();
+    console.log(music.volume())
+
 });
 
+music.play();
+console.log(music.volume(Number(volume())))
 
