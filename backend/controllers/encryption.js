5e5d272457b908ea6052d7889153076a0dd452d9
Nichzar <74956401+Nichlas-C@users.noreply.github.com>
2025-04-09 16:05:44 +0200

    encryption and compareison of passwords (#164)

---
+++ backend/controllers/encryption.js
@@ -0,0 +1,17 @@
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
+
+export async function hashPassword(password) {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    console.log('Hashed Password:', hashedPassword);
+    return hashedPassword;
+}
+
+export async function comparePassword(storedHashedPassword, passwordToCheck) {
+    const match = await bcrypt.compare(passwordToCheck, storedHashedPassword);
+    if (match) {
+        return match;
+    } else {
+        console.log('Password does not match.');
+    }
+}
